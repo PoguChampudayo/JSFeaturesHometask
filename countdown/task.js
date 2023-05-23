c8764bce63a115a6a1ae7039a3427a7d81f5c8bb
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-05-23 11:43:15 +0500

    added countdown

---
+++ countdown/task.js
@@ -0,0 +1,9 @@
+const timer = document.getElementById('timer')
+const time = new Date(1970, 0, 1, ...timer.textContent.split(':'))
+internalTimer = setInterval(() => {
+    time.setTime(time.getTime() - 1000)
+    timer.textContent = `${String(time.getHours()).padStart(2, '0')}:${String(time.getMinutes()).padStart(2, '0')}:${String(time.getSeconds()).padStart(2, '0')}`
+}, 1000)
+setTimeout(() => { clearInterval(internalTimer); alert('Вы победили в конкурсе!'); }, time.getTime() + 18000000)
+
+
